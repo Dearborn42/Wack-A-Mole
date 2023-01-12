3aab0c3eb61bb090bb0099298a91acc9eda46fec
AndrewMurphy <amurf26@outlook.com>
2023-01-11 17:43:51 -0700

    made random generator

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-// var c = document.getElementById("game");
-// var ctx = c.getctx("2d");
+var c = document.getElementById("game");
+var ctx = c.getContext("2d");
 
 let layout = Array(2);
 for(let i=0; i < layout.length; i++) {
@@ -7,12 +7,29 @@ for(let i=0; i < layout.length; i++) {
 }
 
 class Yoda{
+    createGame(){
+        this.layout = 
+        [
+            [null, null, null],
+            [null, null, null]
+        ]
+        return this;
+    }
     setRandNum(){
         this.x = Math.floor(Math.random()*3);
-        this.y = Math.floor(Math.random()*2);
+        this.y = Math.floor(Math.random()*2)
+        this.layout[this.y][this.x] = "yoda";
         return this;
+    }
+    placeYoda(){
+        ctx.beginPath();
+        ctx.fillStyle = "green";
+        ctx.fillRect(this.x*200, this.y*200, 200, 200);
+        ctx.stroke();
     }
 }
 
 let test = new Yoda();
-console.log(test.setRandNum());
+test.createGame().setRandNum();
+console.log(test.x, test.y);
+console.log(test.createGame().setRandNum().placeYoda());
