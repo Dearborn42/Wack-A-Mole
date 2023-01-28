74c9a41c3ea3cae32f247f39774645c760159f3c
Dearborn42 <amurf26@outlook.com>
2023-01-27 22:59:11 -0700

    started second gamemode

--- main.js
+++ main.js
@@ -7,47 +7,86 @@
 var mode = null; var timer = 0; var check = 0; var highScore = 0; var score = 0;
 const holeElements = document.querySelectorAll('.hole');
 
-class Yoda {
+class Yoda{
     constructor(){
         this.hole;
         this.index;
+        this.active;
     }
     yodaAppear(){
         this.index = Math.floor(Math.random() * holeElements.length);
         this.hole = holeElements[this.index];
+        this.hole.classList.remove('down');
         this.hole.classList.add('up');
         return this;
     }
+    yodaDisappear(){
+        for(let i=0; i<holeElements.length; i++){
+            this.hole = holeElements[i];
+            this.hole.classList.remove('up');
+            this.hole.classList.add('down');
+        }
+        return this;
+    }
     yodaHit(){
-        this.hole.classList.remove('up');
-        score++;
+        this.yodaDisappear();
+        this.index = 111;
+        timer = 0;check = 0;score++;
         document.getElementById('score').innerHTML = "Score: " + score;
     }
+    yodaMissed(){
+        check++;
+        if(check == 2)
+            this.endGame();
+    }
+    removeButton(){
+        document.getElementById('m1').hidden = true;
+        document.getElementById('m2').hidden = true;
+    }
+    endGame(){
+        for(let i=0; i<99999; i++)window.clearTimeout(i);
+        this.active = false;
+        mode = null;timer = 0;check = 0;
+        if(score > highScore)highScore = score;
+        score = 0;
+        this.yodaDisappear();
+        document.getElementById('score').innerHTML = "Score: 0";
+        document.getElementById('highScore').innerHTML = "Highscore: " + highScore;
+        document.getElementById('m1').hidden = false;
+        document.getElementById('m2').hidden = false;
+    }
 }
+
 
 let yodaGame = new Yoda();
 
 function hit(mound){
-    if(parseInt(mound, 10) == yodaGame.index){
-        yodaGame.yodaHit();
+    if(mode==true)parseInt(mound, 10) === yodaGame.index ? yodaGame.yodaHit() : yodaGame.yodaMissed();
+}
+function test(){
+    if(mode == true){
+        yodaGame.removeButton();
+        yodaGame.active = true;
+        yodaGame.yodaDisappear().yodaAppear();
+        timer++;
+        if(timer == 3)yodaGame.endGame();
+        if(yodaGame.active)setTimeout(test, 2000);
     }
 }
 
-function test(){
-    yodaGame.yodaAppear();
+
+
+
+let yodaGame2 = new Yoda();
+
+var pos;
+function getPos(num){
+    if(mode == false)pos = parseInt(num, 10);
+    let index = Math.floor(Math.random() * holeElements.length);
+    if(index != pos){
+        score++;
+        document.getElementById('score').innerHTML = "Score: " + score;
+    }else{
+        yodaGame2.endGame();
+    }
 }
-
-// Make sure the image is loaded first otherwise nothing will draw.
-// gets x and y coordinates of mouse and uses them to dectect if you have hit or missed the square
-// if hit you get more score
-// if miss twice then game ends
-// var time = 2000/hits;
-
-function getPos(pos){}
-
-// mode 1 is basic wack-a-mole
-function mode1(){}
-// creates random starting position
-function create(){}
-
-function mode2(){}
